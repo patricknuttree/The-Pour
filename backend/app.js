49b850bf1cc3694705d06399de12724a88dcdab9
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-04-22 13:48:46 -0400

    Add base error handlers

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -34,5 +35,53 @@ app.use(
 )
 
 app.use(routes);
+//ROUTES
+
+
+
+
+
+
+
+//PUT ROUTES ABOVE THIS LINE.
+//ERROR HANDLING
+  //Catch unhandled requests and forward to error handler. aka request not found 
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+  //Process sequelize Errors
+app.use((err, _req, _res, next) => {
+  if(err instanceof ValidationError){
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  };
+  next(err)
+});
+
+//ERROR FORMATTER
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+//DO NOT ADD ADDITIONAL ERROR HANDLERS BELOW THIS LINE. PUT THEM ABOVE ERROR FORMATTER. 
+
+
+
+
+
+
+
+
 
 module.exports = app;
