43f50f2efb13f6f2b61f280b031a01289599d824
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-04-22 18:16:48 -0400

    Add Session and users file to api folder along with a log in route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,66 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users');
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter)
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//STUFF FOR TESTING DURING INITIAL SET UP. 
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+
+// router.get('/set-token-cookie', asyncHandler(async (rea, res) => {
+//   const user = await User.findOne({
+//     where:{
+//       username: 'Demo-lition'
+//     },
+//   })
+//   setTokenCookie(res, user);
+//   return res.json({ user })
+// }))
+
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
+// router.post('/test', function(req, res) {
+//   res.json({ requestBody: req.body });
+// });
+
 
 
 
