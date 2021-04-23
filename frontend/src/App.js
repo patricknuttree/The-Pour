f511bd3230c03e6d532c0e24a0e9afa1838af82c
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-04-23 16:35:54 -0400

    add signup form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch } from "react-redux"
-import { Route, Switch } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage'
-import * as sessionActions from "./store/session"
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
+import * as sessionActions from "./store/session";
 
 function App() {
   const dispatch = useDispatch();
@@ -10,10 +11,14 @@ function App() {
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
+
   return isLoaded && (
     <Switch>
-      <Route path='/login'>
+      <Route path="/login">
         <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
       </Route>
     </Switch>
   );
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -52,6 +52,8 @@ export const signup = (user) => async (dispatch) => {
   return response;
 }
 
+
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
