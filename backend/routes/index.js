8b26e77f78c32ac564411036334ef488daae0c71
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-04-25 01:44:31 -0400

    Bug Fixes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,31 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
+if(process.env.NODE_ENV === 'production'){
+  const path = require('path');
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
+if(process.env.NODE_ENV !== 'production'){
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
 
 //TEST ROUTE FOR SETTING UP BACKEND. 
 // router.get('/hello/world', function(req, res) {
