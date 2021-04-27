8812f9601671bc4d6dbb530f61b9387f6d5526b7
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-04-27 18:24:10 -0400

    added Demo button

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,6 +1,12 @@
 express = require('express');
 router = express.Router();
+const asyncHandler = require('express-async-handler');
 
 const db = require('../db/models');
 const { User, Whiskey} = db;
+
+router.get('/'), asyncHandler( async(req, res, next) => {
+  const userWhiskey = Whiskey.findAll();
+  return res.json(userWhiskey);
+})
 
