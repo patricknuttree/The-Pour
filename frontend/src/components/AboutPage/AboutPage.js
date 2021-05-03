fe28b818e2e74484f011cc2f4a60d0ce7fb5ac33
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-05-02 22:00:42 -0400

    Added Favicon, added the github, and linkedin links to the about page.

--- frontend/src/components/AboutPage/AboutPage.js
+++ frontend/src/components/AboutPage/AboutPage.js
@@ -7,7 +7,9 @@ const AboutPage = () => {
       <h2>Developer</h2>
       <h3>Patrick Nusbaum</h3>
       <img src="https://secure.gravatar.com/avatar/e0dfd8561a1c14f21173e109dca1d69d?secure=true&size=300" />
- 
+      <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/patrick-nusbaum-mpa">Linkedin</a>
+      <a target="_blank" rel="noreferrer" href="https://github.com/patricknuttree">github</a>
+
     </div>
   )
 
